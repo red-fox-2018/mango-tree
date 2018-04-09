8faddea533e54efe610547243d76b4fdaf314f85
David Joshua <davidjosh_87@yahoo.com>
2018-04-09 22:06:30 +0700

    soccer-afif

---
+++ soccer/league.js
@@ -0,0 +1,45 @@
+/*jshint esversion:6*/
+/*jshint -W097*/
+/*jshint -W117*/
+/*jshint -W030*/
+
+//League
+class League {
+  constructor(country, ceo){
+    this.country = country;
+    this.ceo = ceo;
+    this.clubs = [];
+  }
+
+  addClub(club){
+    this.clubs.push(club);
+  }
+
+  members(){
+    console.log('List club in this League :');
+    console.log('===============================');
+
+    for(let i = 0; i < this.clubs.length; i++){
+      console.log(`${i+1}. ${this.clubs[i].name}, stadium: ${this.clubs[i].stadium}`);
+    }
+  }
+
+}
+
+//League's Child
+class PremierLeague extends League {
+  constructor(){
+    super('United Kingdom', 'Richard Scudamore');
+  }
+}
+
+class LaLiga extends League {
+  constructor(){
+    super('Spain', 'Javier Gomez');
+  }
+}
+
+module.exports = {
+  PremierLeague,
+  LaLiga
+};
