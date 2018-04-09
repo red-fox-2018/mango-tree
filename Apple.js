345f29f2135716fbf0a3553728e7ae56b9b8d8ba
Iswanul Umam <umam@sepulsa.com>
2018-04-09 19:50:11 +0700

    finish release 2

--- Apple.js
+++ Apple.js
@@ -1,7 +1,9 @@
 
 
 class Apple {
-  
+  constructor() {
+    this._quality = Math.round(Math.random()) == 1 ? 'good' : 'bad';
+  }
 }
 
 module.exports = Apple;
--- index.js
+++ index.js
@@ -3,12 +3,12 @@
  */
 
 const MangoTree = require('./MangoTree');
+const AppleTree = require('./AppleTree');
 
 // # Release 0
 let mangoTree = new MangoTree(3, 10, 15);
-
+console.log(`# Mango Tree`);
 console.log(`# The tree is alive!`);
-
 do {
   mangoTree.grow();
   // mangoTree.produceMangoes();
@@ -18,3 +18,13 @@ do {
 console.log(`# The tree has met its end.`);
 
 // # Release 1
+let appleTree = new AppleTree(4, 9, 20);
+console.log(`# Apple Tree`);
+console.log(`# The tree is alive!`);
+do {
+  appleTree.grow();
+  // appleTree.produceMangoes();
+  appleTree.harvest();
+  console.log(`[Year ${appleTree.age} Report] Height = ${appleTree.height} | Fruits harvested = ${appleTree.harvested}`);
+} while (appleTree.healtyStatus != false);
+console.log(`# The tree has met its end.`);
