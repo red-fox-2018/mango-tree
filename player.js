265f1da85a1518e7cc650b2b6a339898e24acec4
OkyWiliarso <okywiliarso@gmail.com>
2018-04-09 20:23:00 +0700

    done

---
+++ player.js
@@ -0,0 +1,14 @@
+//Player
+ class Player {
+   constructor(playerName, height, position, nationality, transferFee){
+     this.playerName = playerName
+     this.height = height
+     this.position = position
+     this.nationality = nationality
+     this.transferFee = transferFee
+     this.currentClub = null
+   }
+
+ }
+
+module.exports = Player
