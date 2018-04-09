a483849569ebe0884ead669415f3be09f5b403f4
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-09 20:05:01 +0700

    make league DRY

--- league.js
+++ league.js
@@ -6,39 +6,28 @@ class League {
         this.ceo = ceo;
         this.clubs = clubs;
     }
+
+    addClub(obj) {
+        this.clubs.push(obj);
+    }
+
+    members() {
+        console.log('List club in this League:')
+        this.clubs.forEach((club, i) => {
+            console.log(`${i + 1}. ${club.name}, ${club.stadium}`)
+        });
+    }
 }
 
 class PremierLeague extends League {
     constructor() {
         super('United Kingdom', 'Richard Scudamore', []);
     }
-
-    addClub(obj) {
-        this.clubs.push(obj);
-    }
-
-    members() {
-        console.log('List club in this League:')
-        this.clubs.forEach((club, i) => {
-            console.log(`${i + 1}. ${club.name}, ${club.stadium}`)
-        });
-    }
 }
 
 class LaLiga extends League {
     constructor() {
         super('Spain', 'Javier Gomez', []);
-    }
-
-    addClub(obj) {
-        this.clubs.push(obj);
-    }
-
-    members() {
-        console.log('List club in this League:')
-        this.clubs.forEach((club, i) => {
-            console.log(`${i + 1}. ${club.name}, ${club.stadium}`)
-        });
     }
 }
 
